0cf1437fc6da6e83caff43dbb4f80f1d788de1c2
XavierAmico <christianxamico@gmail.com>
2026-02-24 13:34:30 -0600

    Making Models and Login

--- newApp/views.py
+++ newApp/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html',{})
+
+def register(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			message.success(request, "Account created successfully!")
